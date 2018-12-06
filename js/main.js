2146b4293075cd38a80117a06a8efec0a4245d94
Andrew Kristanto <andrewkristanto555@gmail.com>
2018-12-06 17:20:43 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,59 @@
+var users = new Map();
+users.set('Andrew', 'Kristanto');
+
+function loadLogin(){
+  console.log('swapping to login page');
+  location.href = "./login.html";
+};
+
+function loadRegistration(){
+  console.log('swapping to registration page');
+  location.href = "./register.html";
+};
+
+function loadWelcome(){
+  console.log('swapping to welcome page');
+  location.href = "./welcome.html";
+}
+
+function loadMain(){
+  console.log('swapping to main page');
+  location.href = "./main.html";
+}
+
+function verify_login(){
+  console.log('verfying login');
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  if (users.has(username)) {
+    if (users.get(username) == password) {
+      console.log('login success');
+      loadMain();
+    } else {
+      alert("Incorrect password.");
+    }
+  } else {
+    alert("User does not exist.");
+  }
+}
+
+function register(){
+  console.log('registering');
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  var password2 = document.getElementById('password2').value;
+  var type = document.getElementById('type').value;
+  if (users.has(username)){
+    alert("Username already exists.");
+  } else if (username.length == 0) {
+    alert("Username cannot be null.");
+  } else if (password.length == 0) {
+    alert("Password cannot be null.");
+  } else if(password != password2) {
+    alert("Passwords do not match.");
+  } else {
+    console.log('register success');
+    users.set(username, password);
+    loadWelcome();
+  }
+}
